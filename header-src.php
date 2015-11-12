b400f53ff7f8afca46df9cbbd7fe0c96e928c280
justmarkup <hallo@justmarkup.com>
2015-11-12 16:51:33 +0100

    add link to open redesign post in banner

--- header-src.php
+++ header-src.php
@@ -50,6 +50,7 @@
 </head>
 <body <?php body_class(); ?>>
 	<header class="header"role="banner">
+		<p class="banner">This site is undergoing an <a href="https://justmarkup.com/log/2015/11/open-redesign/">open redesign</a>. See the <a href="https://github.com/justmarkup/justmarkup.com">Code</a> and open <a href="https://github.com/justmarkup/justmarkup.com/issues">Issues</a>.</p>
 		<h1><a class="logo" href="<?php echo get_home_url(); ?>">justmarkup</a></h1>
 		<nav class="nav" role="navigation">
 			<?php wp_nav_menu(); ?> 
