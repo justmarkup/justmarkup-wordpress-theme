7bc3ed7a348312729c290638555b074f3d722f98
justmarkup <hallo@justmarkup.com>
2016-02-19 14:03:04 +0100

    add blockquote styles and theme color

--- header-src.php
+++ header-src.php
@@ -14,7 +14,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-
+<meta name="theme-color" content="rgba(39,174,96,0.6)" />
 <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/manifest.json" />
 <title><?php
 	/*
